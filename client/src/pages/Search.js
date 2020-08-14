51841a5fe1ccd51f0fdbe29926c7a834b8c0c1de
eddyangang <eddyyang@berkeley.edu>
2020-08-14 12:01:30 -0700

    resolved conglicts

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,10 +1,11 @@
 import React from "react";
 // import "./style.css";
 import Navbar from "../components/Navbar/index"
-
+import Banner from "../components/Banner"
 function Search() {
   return (
 <div>
+    <Banner/>
     this is search
 </div>
 
