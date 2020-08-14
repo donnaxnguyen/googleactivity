e321882b08a5cbb78d517300459332e64f76fce7
CofChips <qiwei.mod@gmail.com>
2020-08-14 13:37:14 -0700

    created buttons

---
+++ client/src/components/ButtonsSave/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function ButtonsSave() {
+  return (
+<div>
+<button class="btn btn-primary" type="submit">View</button>
+<button class="btn btn-primary" type="submit">Delete</button>
+</div>
+  );
+}
+
+
+export default ButtonsSave;
---
+++ client/src/components/ButtonsSearch/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function ButtonsSearch() {
+  return (
+<div>
+<button class="btn btn-primary" type="submit">View</button>
+<button class="btn btn-primary" type="submit">Save</button>
+</div>
+  );
+}
+
+
+export default ButtonsSearch;
